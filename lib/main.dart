cd166596e04732a418bd0897e80bd271e95c7312
Irwing Herrera <irwing-herrera-2b@hotmail.com>
2019-02-24 18:15:30 -0600

    Actulización de Titulo

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
  Widget build(BuildContext context) {
   return new Scaffold(
     appBar: new AppBar(
-      title: new Text("AppBar"),
+      title: new Text("AppBar Flutter"),
       backgroundColor: Colors.indigoAccent,
     ),  
     body: new Container(
